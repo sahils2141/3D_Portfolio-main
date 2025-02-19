5dc782ff18f0a54500cd29a4459223efc0e26d89
Sahil Sharma <120122343+sahils2141@users.noreply.github.com>
2025-02-19 10:33:40 +0530

    Update initialFX.ts

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -1,24 +1,24 @@
 import { SplitText } from "gsap-trial/SplitText";
 import gsap from "gsap";
-import { smoother } from "../Navbar";
 
 export function initialFX() {
   document.body.style.overflowY = "auto";
-  smoother.paused(false);
   document.getElementsByTagName("main")[0].classList.add("main-active");
+
   gsap.to("body", {
     backgroundColor: "#0b080c",
     duration: 0.5,
     delay: 1,
   });
 
-  var landingText = new SplitText(
+  const landingText = new SplitText(
     [".landing-info h3", ".landing-intro h2", ".landing-intro h1"],
     {
       type: "chars,lines",
       linesClass: "split-line",
     }
   );
+
   gsap.fromTo(
     landingText.chars,
     { opacity: 0, y: 80, filter: "blur(5px)" },
@@ -35,7 +35,7 @@ export function initialFX() {
 
   let TextProps = { type: "chars,lines", linesClass: "split-h2" };
 
-  var landingText2 = new SplitText(".landing-h2-info", TextProps);
+  const landingText2 = new SplitText(".landing-h2-info", TextProps);
   gsap.fromTo(
     landingText2.chars,
     { opacity: 0, y: 80, filter: "blur(5px)" },
@@ -61,6 +61,7 @@ export function initialFX() {
       delay: 0.8,
     }
   );
+
   gsap.fromTo(
     [".header", ".icons-section", ".nav-fade"],
     { opacity: 0 },
@@ -72,16 +73,16 @@ export function initialFX() {
     }
   );
 
-  var landingText3 = new SplitText(".landing-h2-info-1", TextProps);
-  var landingText4 = new SplitText(".landing-h2-1", TextProps);
-  var landingText5 = new SplitText(".landing-h2-2", TextProps);
+  const landingText3 = new SplitText(".landing-h2-info-1", TextProps);
+  const landingText4 = new SplitText(".landing-h2-1", TextProps);
+  const landingText5 = new SplitText(".landing-h2-2", TextProps);
 
   LoopText(landingText2, landingText3);
   LoopText(landingText4, landingText5);
 }
 
 function LoopText(Text1: SplitText, Text2: SplitText) {
-  var tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+  const tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
   const delay = 4;
   const delay2 = delay * 2 + 1;
 
@@ -95,8 +96,7 @@ function LoopText(Text1: SplitText, Text2: SplitText) {
       y: 0,
       stagger: 0.1,
       delay: delay,
-    },
-    0
+    }
   )
     .fromTo(
       Text1.chars,
@@ -107,8 +107,7 @@ function LoopText(Text1: SplitText, Text2: SplitText) {
         y: 0,
         stagger: 0.1,
         delay: delay2,
-      },
-      1
+      }
     )
     .fromTo(
       Text1.chars,
@@ -119,8 +118,7 @@ function LoopText(Text1: SplitText, Text2: SplitText) {
         ease: "power3.inOut",
         stagger: 0.1,
         delay: delay,
-      },
-      0
+      }
     )
     .to(
       Text2.chars,
@@ -130,7 +128,6 @@ function LoopText(Text1: SplitText, Text2: SplitText) {
         ease: "power3.inOut",
         stagger: 0.1,
         delay: delay2,
-      },
-      1
+      }
     );
 }
